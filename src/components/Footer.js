2d43419ff212321deeaec124c5638b88cb1e1233
ronald <ronaldjunior2@hotmail.com>
2021-05-11 15:38:43 -0300

    preparando review

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function(){
-    
-    return(
-        <div class="inferior">
-            <div class="fechar_pedido" id="desabilitado"><p>Selecione os 3 itens para fechar o pedido</p></div>
-            <div class="fechar_pedido verde oculto" id="habilitado" ><a href="#">Fechar pedido</a></div>
-        </div>
-    )
-}
--- src/components/Topo.js
+++
@@ -1,8 +0,0 @@
-export default function Topo(){
-    return(
-        <div class="topo">
-            <div class="titulo">FoodCamp</div>
-            <div class="subtitulo">Sua comida em 6 minutos</div>
-        </div>
-    )
-}
